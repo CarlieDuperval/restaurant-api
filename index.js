675e493c4db31ae1d0d0bb9e61c4f72bb5744bf2
Carlie Duperval <carlierichell@gmail.com>
2022-05-22 12:39:09 -0400

    Uddated restaurant api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import { addRestaurant, deleteRestaurant, getAllRestaurants, getRestaurantById, updateRestaurant } from './src/ restaurants.js';
+import { getAllRestaurants, addRestaurant } from './src/restaurant.js';
+
+
 const app = express(); // initialize express app
 app.use(cors());   // 
 app.use(express.json()); // allows patch in json format when using potman to CRUD
@@ -8,17 +10,18 @@ app.use(express.json()); // allows patch in json format when using potman to CRU
 
 
 
-
-
-
 // Routes Go Here ------- > Define witch requests are allowed
-app.post('/restaurants', addRestaurant);
 app.get('/restaurants', getAllRestaurants);
-app.get('/restaurants/:restaurantId', getRestaurantById)
-app.patch('/restaurants/:restaurantId', updateRestaurant)
-app.delete('/restaurants/:restaurantId', deleteRestaurant)
+app.post('/restaurants', addRestaurant);
 
 
 app.listen(3000, () => {
     console.log('Listening on port http://localhost:3000...')
-})
+});
+
+
+
+
+
+
+
--- src/connectDb.js
+++ src/connectDb.js
@@ -1,32 +1,25 @@
 //import tools from firebase
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { initializeApp, getApps, cert} from "firebase-admin/app";
 
 // import firestore from firebase
 import { getFirestore } from 'firebase-admin/firestore';
 
-// import my credentials to  connect to firebase
-import myCredentials from '../credentials.js'
+// // import my credentials to  connect to firebase
+ import myCredentials from '../credentials.js'
 
 
-// Write a function to connect to Firebase
-export default function connectDb () {
-  // First Check  to see if we are already connected to the database (Firebase), if we are not connected the array will be empty
+// // Write a function to connect to Firebase
+ export default function connectDb () {
+//   // First Check  to see if we are already connected to the database (Firebase), if we are not connected the array will be empty
     if(getApps().length === 0){  
-        // if not connect , Please connect to firebase project "my-first-firebase-project-cd"
-        initializeApp({     
-            credential: cert(myCredentials) 
+//         // if not connect , Please connect to firebase project "my-first-firebase-project-cd"
+         initializeApp({     
+             credential: cert(myCredentials) 
         })
-    }
-     // Either way ....
-    // return the connection to Firestore
+   }
+//      // Either way ....
+//     // return the connection to Firestore
     return getFirestore();  
 }
 
-// export default function connectDb () {
-//     if(getApps().length === 0){
-//         initializeApp({
-//             credential: cert(myCredentials)
-//         })
-//     }
-//     return getFirestore();  
-// }
+
